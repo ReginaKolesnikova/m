a4db50369d904faa0c347819475dab758b1c250c
ReginaKolesnikova <151017512+ReginaKolesnikova@users.noreply.github.com>
2025-01-30 10:34:10 +0200

    Update localhost_film.sql

--- Andmebaasid/localhost_film.sql
+++ Andmebaasid/localhost_film.sql
@@ -4,3 +4,15 @@ filmNimetus varchar(30) not null,
 aasta int,
 eelarveHind decimal(7,1)
 );
+
+INSERT INTO film(filmNimetus, aasta, eelarveHind)
+Values('Titanic', 2024, 55555.5)
+
+Create table zanr(
+zanrID int primary key AUTO_INCREMENT,
+zanrNimetus varchar(20) unique
+);
+
+Insert into zanr(zanrNimetus)
+values ('draama'), ('detektiiv');
+select * from zanr;
